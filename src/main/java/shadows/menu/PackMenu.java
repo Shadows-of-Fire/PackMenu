79a76416bdc05ed44f6e3a6ff1aa89c70c20d370
Shadows_of_Fire <Bward7864@gmail.com>
2020-08-06 22:26:02 -0400

    register displaytest extension

--- src/main/java/shadows/menu/PackMenu.java
+++ src/main/java/shadows/menu/PackMenu.java
@@ -1,11 +1,15 @@
 package shadows.menu;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(PackMenu.MODID)
 public class PackMenu {
@@ -17,6 +21,7 @@ public class PackMenu {
 		if (FMLEnvironment.dist == Dist.CLIENT) {
 			new PackMenuClient().load();
 		} else LOGGER.error("Running on a dedicated server, disabling mod.");
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 	}
 
 }
