5ffc6bb5f409f6a92716fc432442294215e1145b
Shadows_of_Fire <Bward7864@gmail.com>
2021-03-01 17:52:13 -0500

    Closes #24

--- src/main/java/shadows/menu/buttons/ScreenType.java
+++ src/main/java/shadows/menu/buttons/ScreenType.java
@@ -7,8 +7,10 @@ import net.minecraft.client.gui.AccessibilityScreen;
 import net.minecraft.client.gui.screen.LanguageScreen;
 import net.minecraft.client.gui.screen.MultiplayerScreen;
 import net.minecraft.client.gui.screen.OptionsScreen;
+import net.minecraft.client.gui.screen.PackScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.WorldSelectionScreen;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.fml.client.gui.screen.ModListScreen;
 
 public enum ScreenType implements Function<Screen, Screen> {
@@ -18,7 +20,8 @@ public enum ScreenType implements Function<Screen, Screen> {
 	LANGUAGE(
 			m -> new LanguageScreen(m, Minecraft.getInstance().gameSettings, Minecraft.getInstance().getLanguageManager())),
 	OPTIONS(m -> new OptionsScreen(m, Minecraft.getInstance().gameSettings)),
-	ACCESSIBILITY(m -> new AccessibilityScreen(m, Minecraft.getInstance().gameSettings));
+	ACCESSIBILITY(m -> new AccessibilityScreen(m, Minecraft.getInstance().gameSettings)),
+	RESOURCE_PACKS(new RPScreenFunction());
 
 	protected Function<Screen, Screen> supplier;
 
@@ -29,6 +32,18 @@ public enum ScreenType implements Function<Screen, Screen> {
 	@Override
 	public Screen apply(Screen t) {
 		return supplier.apply(t);
+	}
+
+	public static class RPScreenFunction implements Function<Screen, Screen> {
+
+		@Override
+		public PackScreen apply(Screen t) {
+			Minecraft mc = Minecraft.getInstance();
+			OptionsScreen optScn = new OptionsScreen(t, mc.gameSettings);
+			optScn.init(mc, 40, 40);
+			return new PackScreen(t, mc.getResourcePackList(), optScn::func_241584_a_, mc.getFileResourcePacks(), new TranslationTextComponent("resourcePack.title"));
+		}
+
 	}
 
 }
